3e3f86b2e0c678e183661aad6745eea345f5ebcf
vidisha-goel <vidisha.goel@paytm.com>
2017-06-07 12:47:50 +0530

    update checksum.js

--- paytm/checksum.js
+++ paytm/checksum.js
@@ -9,19 +9,24 @@ var crypto = require('crypto');
 function paramsToString(params, mandatoryflag) {
   var data = '';
   var tempKeys = Object.keys(params);
+  tempKeys.sort();
   tempKeys.forEach(function (key) {
--	var n = params[key].includes("REFUND");	
-	
- -	var m = params[key].includes("|");			
- -		if(n == false || m == false)		
- -		{		
+  var n = params[key].includes("REFUND"); 
+   var m = params[key].includes("|");  
+        if(n == true )
+        {
+          params[key] = "";
+        }
+          if(m == true)
+        {
+          params[key] = "";
+        }  
     if (key !== 'CHECKSUMHASH' ) {
       if (params[key] === 'null') params[key] = '';
       if (!mandatoryflag || mandatoryParams.indexOf(key) !== -1) {
         data += (params[key] + '|');
       }
     }
-  }
 });
   return data;
 }
@@ -102,17 +107,19 @@ crypt.gen_salt(4, function (err, salt) {
 function paramsToStringrefund(params, mandatoryflag) {
   var data = '';
   var tempKeys = Object.keys(params);
+  tempKeys.sort();
   tempKeys.forEach(function (key) {
- -	var m = params[key].includes("|");			
- -		if(m == false)		
- -		{		
+   var m = params[key].includes("|");  
+          if(m == true)
+        {
+          params[key] = "";
+        }  
     if (key !== 'CHECKSUMHASH' ) {
       if (params[key] === 'null') params[key] = '';
       if (!mandatoryflag || mandatoryParams.indexOf(key) !== -1) {
         data += (params[key] + '|');
       }
     }
-  }
 });
   return data;
 }
