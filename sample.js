c1dd3a9c51c3df8d129c666a421d5da324dd0618
mhdarshad <38720792+mhdarshad@users.noreply.github.com>
2021-07-31 18:26:26 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -14,10 +14,10 @@ paytmParams["ORDERID"] = "YOUR_ORDER_ID_HERE";
 * Generate checksum by parameters we have
 * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
 */
-var paytmChecksum = PaytmChecksum.generateSignature(paytmParams, "YOUR_MERCHANT_KEY");
+var paytmChecksum = PaytmChecksum.generateSignature(paytmParams, "NJGKEi15196560300156");
 paytmChecksum.then(function(result){
 	console.log("generateSignature Returns: " + result);
-	var verifyChecksum =  PaytmChecksum.verifySignature(paytmParams, "YOUR_MERCHANT_KEY",result);
+	var verifyChecksum =  PaytmChecksum.verifySignature(paytmParams, "1001",result);
 	console.log("verifySignature Returns: " + verifyChecksum);
 }).catch(function(error){
 	console.log(error);
